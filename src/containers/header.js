628c95cf0f385560b40e32b2fb4ea35be1ec5733
Kim Litjens <Kimlitjens4@gmail.com>
2021-04-28 10:38:47 +0200

    add craco and fixed tailwind bug

--- src/containers/header.js
+++ src/containers/header.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export function HeaderContainer({ children }) {
     return (
-        <header>
+        <header classname="">
             <div className="logo  ">
                 {/* <img src={"/images/kim.jpg"} alt="Kim" className="h-32" /> */}
             </div>
@@ -10,7 +10,7 @@ export function HeaderContainer({ children }) {
                 <span className="hamburger"></span>
             </button>
             <nav className="nav">
-                <ul className="navList">
+                <ul className="navList bg-red-500">
                     <li className="navItem"><a href="#home" className="navLink"></a>Home</li>
                     <li className="navItem"><a href="#services" className="navLink">My Services</a></li>
                     <li className="navItem"><a href="#about" className="navLink">About me</a></li>
