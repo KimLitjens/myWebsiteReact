f93eb8141a6042e6300326d2002c67b138a5178e
Kim Litjens <Kimlitjens4@gmail.com>
2021-05-08 11:02:40 +0200

    add fontawesom

--- src/containers/footer.js
+++ src/containers/footer.js
@@ -1,12 +1,18 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub, faLinkedinIn } from '@fortawesome/free-brands-svg-icons'
 
 export function FooterContainer({ children }) {
     return (
         <footer id="footer">
             <a href="mailto:myMailAdres@gmail.com" className="footerLink">My mailadres</a>
             <ul className="socialList">
-                <li className="socialListItem">Github</li>
-                <li className="socialListItem">LinkedIn</li>
+                <li className="socialListItem">
+                    <FontAwesomeIcon icon={faGithub} /> Github
+                </li>
+                <li className="socialListItem flex">
+                    <FontAwesomeIcon icon={faLinkedinIn} /> <p className="ml-1">Linked</p><p className="bg-blue-500 text-light relative px-1">In</p>
+                </li>
                 <li className="socialListItem">Something else</li>
             </ul>
         </footer>
