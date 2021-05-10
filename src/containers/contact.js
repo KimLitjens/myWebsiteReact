01a2809537d377d7b50fadb14b4851bcb4c613a5
Kim Litjens <Kimlitjens4@gmail.com>
2021-05-10 21:27:18 +0200

    basic styling contact

--- src/containers/contact.js
+++ src/containers/contact.js
@@ -3,11 +3,11 @@ import React from 'react';
 
 export function ContactContainer({ children }) {
     return (
-        <section class="container" id="contact">
-            <div class="formBx">
+        <section className="" id="contact">
+            <div className="formBx flex justify-center">
                 <form
                     data-netlify="true"
-                    class="needs-validation"
+                    className="needs-validation"
                     method="post"
                     name="contact"
                     data-netlify-recaptcha="true"
@@ -15,68 +15,69 @@ export function ContactContainer({ children }) {
                     novalidate
                 >
                     <h2>Contact me</h2>
-                    <div class="form-group inputBox">
+                    <div class="relative z-0 w-full mb-5 form-group ">
                         <input
                             type="text"
-                            class="form-control"
-                            id="nameForm"
                             name="name"
+                            placeholder="Enter name "
                             required
+                            class="pt-3 pb-2 block w-full px-0 mt-0 bg-transparent border-0 border-b-2 border-black appearance-none focus:outline-none focus:ring-0 focus:border-accent border-gray-200"
                         />
-                        <span>Full Name</span>
-                        <div class="invalid-feedback">
-                            Please fill in your name.
-                        </div>
+                        {/* <label for="name" class="absolute duration-300 top-3 -z-1 origin-0 text-gray-500">Enter name</label> */}
+                        {/* <span class="text-sm text-red-600 hidden" id="error">Name is required</span> */}
                     </div>
-                    <div class="form-group inputBox">
+                    <div className="form-group inputBox">
                         <input
                             type="email"
                             name="email"
                             id="email"
-                            class="form-control"
+                            className="form-control"
+                            placeholder="Email Address"
+                            class="pt-3 pb-2 block w-full px-0 mt-0 bg-transparent border-0 border-b-2 border-black appearance-none focus:outline-none focus:ring-0 focus:border-accent border-gray-200"
                             required
                         />
-                        <span>Email Address</span>
-                        <div class="invalid-feedback">
-                            Please fill in your email.
-                        </div>
+
                     </div>
-                    <div class="form-group inputBox">
+                    <div className="form-group inputBox">
                         <input
                             type="text"
                             name="subject"
                             id="subject"
-                            class="form-control"
+                            className="form-control"
+                            placeholder="Subject"
+                            class="pt-3 pb-2 block w-full px-0 mt-0 bg-transparent border-0 border-b-2 border-black appearance-none focus:outline-none focus:ring-0 focus:border-accent border-gray-200"
                             required
                         />
-                        <span>Subject</span>
-                        <div class="invalid-feedback">
-                            Please fill in the subject.
-                        </div>
+
                     </div>
-                    <div class="form-group inputBox">
+                    <div className="form-group inputBox mb-2">
                         <textarea
                             name="message"
                             id="message"
                             cols="30"
                             rows="5"
                             required
-                            class="form-control"
+                            className="form-control"
+                            class="pt-3 pb-2 block w-full px-0 mt-0 bg-transparent border-0 border-b-2 border-black appearance-none focus:outline-none focus:ring-0 focus:border-accent border-gray-200"
+                            placeholder="Type Your Message Here..."
                         ></textarea>
-                        <span>Type Your Message Here...</span>
-                        <div class="invalid-feedback">
-                            Please fill in your message.
-                        </div>
+                        <span></span>
                     </div>
                     <div data-netlify-recaptcha="true"></div>
-                    <button
-                        type="submit"
-                        class="btn"
-                        value="SEND MES"
-                        name="submit"
-                    >
-                        SEND
+                    <div className="flex  justify-center">
+                        <button
+                            type="submit"
+                            value="SEND MES"
+                            name="submit"
+                            className="
+                            btn   py-2 px-6 bg-accent text-dark rounded-md cursor-pointer text-sm uppercase tracking-widest font-black
+                            transition duration-200 ease-in-out transform hover:-translate-y-1 hover:scale-110
+                        "
+                        >
+                            SEND
                     </button>
+                    </div>
+
                 </form>
             </div>
         </section>
