3b2d5021b93437a69b4f039e244c5fb024685a6e
Kim Litjens <Kimlitjens4@gmail.com>
2021-05-11 07:32:51 +0200

    basic styled footer

--- src/containers/footer.js
+++ src/containers/footer.js
@@ -4,14 +4,17 @@ import { faGithub, faLinkedinIn } from '@fortawesome/free-brands-svg-icons'
 
 export function FooterContainer({ children }) {
     return (
-        <footer id="footer">
-            <a href="mailto:myMailAdres@gmail.com" className="footerLink">My mailadres</a>
-            <ul className="socialList">
+        <footer className="p-10 bg-dark text-accent" id="footer">
+
+            <ul className="socialList flex inline-blok justify-between">
+                <li className="socialListItem">
+                    <a href="mailto:myMailAdres@gmail.com" className="footerLink">My mailadres</a>
+                </li>
                 <li className="socialListItem">
                     <FontAwesomeIcon icon={faGithub} /> Github
                 </li>
                 <li className="socialListItem flex">
-                    <FontAwesomeIcon icon={faLinkedinIn} /> <p className="ml-1">Linked</p><p className="bg-blue-500 text-light relative px-1">In</p>
+                    <FontAwesomeIcon icon={faLinkedinIn} /> <p className="ml-1">Linkedin</p>
                 </li>
                 <li className="socialListItem">Something else</li>
             </ul>
