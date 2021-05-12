1651a992b06c7a4b1df98bbd1eb333c237c8eeaf
Kim Litjens <Kimlitjens4@gmail.com>
2021-05-12 20:34:54 +0200

    add hamburger & styled header

--- src/containers/header.js
+++ src/containers/header.js
@@ -1,23 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Hamburger from '../components/hamburger'
 
 export function HeaderContainer({ children }) {
+    const [open, setOpen] = useState(false);
+
     return (
-        <header classname="">
-            <div className="logo  ">
-                {/* <img src={"/images/kim.jpg"} alt="Kim" className="h-32" /> */}
+        <header className={`sticky flex justify-between z-50 top-0 ${open && "bg-accent text-dark w-full h-screen transition translate-x-full "}`}>
+            <div className="logo">
+                <p>logo</p>
             </div>
-            <button className="nav-toggle" aria-label="toggle navigation">
-                <span className="hamburger"></span>
-            </button>
-            <nav className="nav hidden">
-                <ul className="navList bg-primary">
-                    <li className="navItem"><a href="#home" className="navLink">Home</a></li>
-                    <li className="navItem"><a href="#services" className="navLink">My Services</a></li>
-                    <li className="navItem"><a href="#about" className="navLink">About me</a></li>
-                    <li className="navItem"><a href="#work" className="navLink">My Work</a></li>
-                    <li className="navItem"><a href="#contact" className="navLink">Contact</a></li>
+            <nav className={`flex ${!open && "hidden"}`}>
+                <ul className="navList bg-primary flex flex-col justify-evenly text-5xl text-center">
+                    <li className="navItem"><a href="#home" className="navLink" onClick={() => setOpen(!open)}>Home</a></li>
+                    <li className="navItem"><a href="#services" className="navLink " onClick={() => setOpen(!open)}>My Services</a></li>
+                    <li className="navItem"><a href="#about" className="navLink " onClick={() => setOpen(!open)}>About me</a></li>
+                    <li className="navItem"><a href="#work" className="navLink " onClick={() => setOpen(!open)}>My Work</a></li>
+                    <li className="navItem"><a href="#contact" className="navLink " onClick={() => setOpen(!open)}>Contact</a></li>
                 </ul>
             </nav>
+            <div>
+                <Hamburger open={open} setOpen={setOpen} />
+            </div>
         </header>
     )
 }
