3b44e366f6cd85d870d7ae53b25a1e014f6435b4
Kim Litjens <Kimlitjens4@gmail.com>
2021-05-13 09:24:14 +0200

    add animation to header

--- src/containers/header.js
+++ src/containers/header.js
@@ -5,22 +5,22 @@ export function HeaderContainer({ children }) {
     const [open, setOpen] = useState(false);
 
     return (
-        <header className={`sticky flex justify-between z-50 top-0 ${open && "bg-accent text-dark w-full h-screen transition translate-x-full "}`}>
-            <div className="logo">
+        <header className="">
+            <div className="fixed z-50 top-0 left-0">
                 <p>logo</p>
             </div>
-            <nav className={`flex ${!open && "hidden"}`}>
-                <ul className="navList bg-primary flex flex-col justify-evenly text-5xl text-center">
-                    <li className="navItem"><a href="#home" className="navLink" onClick={() => setOpen(!open)}>Home</a></li>
-                    <li className="navItem"><a href="#services" className="navLink " onClick={() => setOpen(!open)}>My Services</a></li>
-                    <li className="navItem"><a href="#about" className="navLink " onClick={() => setOpen(!open)}>About me</a></li>
-                    <li className="navItem"><a href="#work" className="navLink " onClick={() => setOpen(!open)}>My Work</a></li>
-                    <li className="navItem"><a href="#contact" className="navLink " onClick={() => setOpen(!open)}>Contact</a></li>
-                </ul>
-            </nav>
-            <div>
+            <div className="fixed z-50 top-0 right-0">
                 <Hamburger open={open} setOpen={setOpen} />
             </div>
+            <nav className={`fixed z-40 bg-accent text-dark w-full ${open ? "transform duration-500 translate-x-0" : "transform duration-500 translate-x-full"}`}>
+                <ul className="navList h-screen flex flex-col justify-evenly text-5xl text-center">
+                    <li className="navItem"><a href="#home" className="navLink hover:text-light" onClick={() => setOpen(!open)}>Home</a></li>
+                    <li className="navItem"><a href="#services" className="navLink hover:text-light" onClick={() => setOpen(!open)}>My Services</a></li>
+                    <li className="navItem"><a href="#about" className="navLink hover:text-light" onClick={() => setOpen(!open)}>About me</a></li>
+                    <li className="navItem"><a href="#work" className="navLink hover:text-light" onClick={() => setOpen(!open)}>My Work</a></li>
+                    <li className="navItem"><a href="#contact" className="navLink hover:text-light" onClick={() => setOpen(!open)}>Contact</a></li>
+                </ul>
+            </nav>
         </header>
     )
 }
