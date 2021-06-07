6da7c9ad9c9c21497d5e6e43a0723f61aba48e18
Kim Litjens <Kimlitjens4@gmail.com>
2021-06-07 14:32:52 +0200

    add GA

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css'
 import App from './App';
+import ReactGA from 'react-ga';
+
+const TRACKING_ID = "UA-168814977-1"; // YOUR_OWN_TRACKING_ID
+ReactGA.initialize(TRACKING_ID);
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 ReactDOM.render(
   <React.StrictMode>
